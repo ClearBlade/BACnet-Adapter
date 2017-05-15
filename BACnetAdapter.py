6c83150314ffb97d680089cd5dedebd455901628
Michael Sprague <msprague@clearblade.com>
2017-05-15 17:12:23 -0400

    work in progress

--- BACnetAdapter.py
+++ BACnetAdapter.py
@@ -1,10 +1,13 @@
 import threading, json
-from bacpypes.app import BIPSimpleApplication
+from bacpypes.app import BIPSimpleApplication, BIPForeignApplication
 from bacpypes.apdu import WhoIsRequest
 from bacpypes.pdu import GlobalBroadcast, Address
 
+from BACnetDevices import BACnetDevices
+from BACnetSensors import BACnetSensors
 from Device import Device
 from MQTT import MQTT
+from clearblade import auth, Client
 
 
 class BACnetAdapter(BIPSimpleApplication):
@@ -16,23 +19,32 @@ class BACnetAdapter(BIPSimpleApplication):
         self.low_limit = args["lowerDeviceIdLimit"]
         self.high_limit = args["upperDeviceIdLimit"]
         self.mqtt = None
-        # todo - create our devices with the provided IPs in the device-config.json file
-        # during device creation we should also get some info on each device, mainly the objectName on the device object
+        self.cb_device_client = None
+        self.bacnet_devices = None
+	self.bacnet_sensors = None
+        self._init_cb()
 
-    def request(self, apdu):
-        if isinstance(apdu, WhoIsRequest):
-            self.who_is_request = apdu
-        BIPSimpleApplication.request(self, apdu)
+    def _init_cb(self):
+        # first authenticate to CB using device auth
+        cb_auth = auth.Auth()
+        if self.cb_device_client is None:
+            self.cb_device_client = Client.DeviceClient(self.credentials['systemKey'], self.credentials['systemSecret'], self.credentials['deviceName'], self.credentials['activeKey'], self.credentials['platformURL'])
+            cb_auth.Authenticate(self.cb_device_client)
+        # init cb mqtt
+        # if self.mqtt is None:
+        #     self.mqtt = MQTT(self.credentials)
+        # init bacnet devices (comes from cb collection)
+        if self.bacnet_devices is None:
+            self.bacnet_devices = BACnetDevices(self.cb_device_client, self)
+        # init bacnet sensors (comes from cb devices table)
+	if self.bacnet_sensors is None:
+	    self.bacnet_sensors = BACnetSensors()
+        # also init bacnet sensor profiles
 
     def do_IAmRequest(self, apdu):
-        if not self.who_is_request:
-            return
-        else:
-            print apdu.iAmDeviceIdentifier
-            device = Device(apdu.iAmDeviceIdentifier, apdu.pduSource, self)
-            device.get_object_list()
+	self.bacnet_devices.got_new_device_who_is_response(apdu)
 
-    def send_props_to_platform(self, device, obj, props):
+    def send_value_to_platform(self, device, obj, props):
         obj_to_send = {
             'device': {
                 'id': device.id,
@@ -50,13 +62,12 @@ class BACnetAdapter(BIPSimpleApplication):
             print e
 
     def start(self):
-        if self.mqtt is None:
-            self.mqtt = MQTT(self.credentials)
-        self.who_is(self.low_limit, self.high_limit, Address("10.16.163.20"))
+        print "in start"
+        #self.who_is(self.low_limit, self.high_limit, Address("10.16.163.20"))
         # todo - here we will want to loop through each device we have, and kick off getting all objects and properties for the device
-        timer = threading.Timer(self.interval, self.start)
-        timer.daemon = True
-        timer.start()
+        #timer = threading.Timer(self.interval, self.start)
+        #timer.daemon = True
+        #timer.start()
 
 
 def json_serial(obj):
